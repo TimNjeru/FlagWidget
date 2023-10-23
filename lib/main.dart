06bde78f9c531ec970deace8c583805a48827e4b
Timothy_Njeru <timothymugendi@gmail.com>
2023-10-23 13:36:03 +0000

    List of countries using flag

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:flag/flag.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Flag Picker',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flag Picker'),
+        ),
+        body: FlagPicker(),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class FlagPicker extends StatefulWidget {
+  const FlagPicker({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
+  _FlagPickerState createState() => _FlagPickerState();
+}
+
+class _FlagPickerState extends State<FlagPicker> {
+  // A model class for the country data
+
+  // A list of country objects with sample data
+  List<Country> countries = [
+    Country('Kenya', 'KE', 'KE'),
+    Country('India', 'IN', 'IN'),
+    Country('Australia', 'AU', 'AU'),
+    Country('France', 'FR', 'FR'),
+    Country('Japan', 'JP', 'JP'),
+    // Add more countries as you wish
+  ];
+
+  // A variable to store the user input
+  String query = '';
+
+  Widget countryItem(Country country) {
+    return ListTile(
+      leading: CircleAvatar(
         child: Text(
-          'Hello, World!',
+          country.flag,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+      title: Text(
+        country.name,
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      subtitle: Text(
+        country.code,
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.grey,
         ),
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // A text field to take the user input
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'Enter country name or code',
+          ),
+          onChanged: (value) {
+            setState(() {
+              query = value;
+            });
+          },
+        ),
+        // A list view to display the filtered list of countries
+        Expanded(
+          child: ListView(
+            children: countries
+                // Filter the countries by name or code containing the query
+                .where((country) =>
+                    country.name.toLowerCase().contains(query.toLowerCase()) ||
+                    country.code.toLowerCase().contains(query.toLowerCase()))
+                // Map each country to a custom list tile widget
+                .map((country) => countryItem(country))
+                .toList(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Country {
+  final String name;
+  final String code;
+  final String flag;
+
+  Country(this.name, this.code, this.flag);
 }
